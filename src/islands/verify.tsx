39512d1e213a5956c6c07a186b7790dc8e6fe7a8
Kin <riennsuu@gmail.com>
2024-04-23 13:55:39 +0000

    fix: disable text selection

--- src/islands/verify.tsx
+++ src/islands/verify.tsx
@@ -4,7 +4,7 @@ interface Data {
 
 export default function Home({ code }: Data) {
 	return (
-		<div class="touch-none font-indieflower grid place-items-center h-dvh select-none">
+		<div class="font-indieflower grid place-items-center h-dvh select-none">
 			<div class="bg-white grid place-items-center relative w-80 h-[345px] rounded-2xl shadow-2xl overflow-hidden">
 				<div class="bg-[url('/sashimi.jpg')] bg-cover bg-center absolute top-0 w-full h-40">
 				</div>
