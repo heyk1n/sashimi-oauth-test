dc79d48d1a6e18ecd5cdf287110dc82c99e4b33e
Kin <riennsuu@gmail.com>
2024-04-26 03:37:51 +0000

    dark theme

--- src/routes/v2.tsx
+++ src/routes/v2.tsx
@@ -1,3 +1,4 @@
+import { asset } from "$fresh/runtime.ts";
 import { Handlers, PageProps } from "$fresh/server.ts";
 
 interface Data {
@@ -16,31 +17,32 @@ export default function Home({ data }: PageProps<Data>) {
 	const { code } = data;
 
 	return (
-		<div class="grid place-items-center h-dvh">
-			<div class="bg-white grid place-items-center relative w-80 h-80 rounded-2xl shadow-2xl overflow-hidden">
-				<div class="bg-[url('/sashimi.jpg')] bg-cover bg-center absolute top-0 w-full h-40">
-				</div>
-				<div class="w-full h-32 px-6 absolute bottom-4">
-					<p class="text-xl">Welcome!!</p>
-					<p class="text-sm text-slate-500">
-						Sebelum masuk, verifikasi akun dulu ya! :3
-					</p>
-					<form>
-						<div class="w-full h-16 grid place-items-center">
-							<input
-								name="code"
-								value={code}
-								type="text"
-								placeholder="XYZXYZ"
-								class="text-center h-10 w-60 bg-gray-200 rounded-lg"
-							>
-							</input>
+		<div class="bg-[#1e1e1e] grid place-items-center h-dvh touch-none p-6">
+			<div class="w-72 rounded-xl overflow-hidden shadow-2xl absolute">
+				<img src="/sashimi.jpg"></img>
+				<div class="font-babydoll w-full p-6 bg-[#141414]">
+					<div class="space-y-4">
+						<div>
+							<p class="font-bold text-2xl text-white">
+								Welcome!!
+							</p>
+							<p class="text-xs text-[#888888]">
+								Sebelum masuk, verifikasi akun kamu terlebih
+								dahulu ya~ :3
+							</p>
 						</div>
-					</form>
-					<div class="w-full h-6 grid place-items-center relative bottom-0">
-						<p class="text-xs text-gray-400 absolute bottom-2">
-							Tekan "Enter" untuk submit
-						</p>
+						<form method="post">
+							<div class="grid place-items-center space-y-4">
+								<input
+									class="bg-black w-full h-10 rounded-lg placeholder:text-white/25 text-center text-white"
+									placeholder="xyzxyz"
+								>
+								</input>
+								<p class="text-[10px] text-slate-500 text-center">
+									Tekan 'Enter' di keyboard mu untuk submit.
+								</p>
+							</div>
+						</form>
 					</div>
 				</div>
 			</div>
